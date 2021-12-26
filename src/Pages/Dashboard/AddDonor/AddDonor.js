97be1705a0dbde67f5516e116a23eda505b5be86
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-12-27 01:31:54 +0600

    add donner route added and database connected

--- src/Pages/Dashboard/AddDonor/AddDonor.js
+++ src/Pages/Dashboard/AddDonor/AddDonor.js
@@ -1,9 +1,56 @@
+import axios from "axios";
 import React from "react";
+import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "./AddDonor.css";
 
 const AddDonor = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    axios.post("http://localhost:5000/donors", data).then((res) => {
+      if (res.data.insertedId) {
+        alert("added Successfully");
+        reset();
+      }
+    });
+  };
   return (
     <div>
-      <h2>Add Donor</h2>
+      <Container>
+        <form className="addDonorForm" onSubmit={handleSubmit(onSubmit)}>
+          <h2>Add Donor</h2>
+          <input
+            className="donorInput"
+            placeholder="Donor Name"
+            type="text"
+            {...register("Name")}
+          />
+          <br />
+          <input
+            className="donorInput"
+            placeholder="Donor Blood Group"
+            type="text"
+            {...register("BloodGroup")}
+          />
+          <br />
+          <input
+            className="donorInput"
+            placeholder="Donor Phone"
+            type="text"
+            {...register("Phone")}
+          />
+          <br />
+          <input
+            className="donorInput"
+            placeholder="Last Donate Date"
+            type="text"
+            {...register("LastDonate")}
+          />
+          <br />
+          <input className="button" type="submit" />
+        </form>
+      </Container>
     </div>
   );
 };
--- src/Pages/Home/OurDonors/OurDonors.js
+++ src/Pages/Home/OurDonors/OurDonors.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
+import { HashLink } from "react-router-hash-link";
 import "./OurDonors.css";
 
 const OurDonors = () => {
@@ -9,6 +11,11 @@ const OurDonors = () => {
           OUR <span>DONORS</span>
         </h2>
       </div>
+      <button className="button py-0 px-2">
+        <Nav.Link className=" text-white" as={HashLink} to="/allDonors">
+          View All
+        </Nav.Link>
+      </button>
     </div>
   );
 };
